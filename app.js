d06808c9001ea9575a61c2d6bbc1a3a7ee21ab67
Andrew Fan <andrewsuicune@gmail.com>
2016-10-22 16:12:09 -0400

    Major refactoring, date comparisons might work smoother assuming nothing's broken

--- app.js
+++ app.js
@@ -17,106 +17,67 @@ function blockPage(id, url) {
 }
 
 /**
+ * Determine whether or not the given page is blocked (i.e. in an ongoing blacklist, not in an ongoing whitelist)
+ */
+function determineIsBlocked(url) {
+    
+}
+
+/**
+ * Determine whether or not the current date is within a event's date range
+ */
+function determineIsEventOngoing(nextevent) {
+    var d = new Date(); //Get current date and time
+    var dateSTART;
+    var dateEND;
+    var eventmonthdate;
+    if (nextevent.repeat) {
+        //determine the start and end dates.
+        eventmonthdate = getRepeatingEventMonthDate(d, nextevent);
+        dateSTART = "" + d.getYear() + " " + zerofillTwoDigits(eventmonthdate[0]) + "" + zerofillTwoDigits(eventmonthdate[1]) + nextevent.startTime;
+        dateEND = "" + d.getYear() + " " + zerofillTwoDigits(eventmonthdate[0]) + "" + zerofillTwoDigits(eventmonthdate[1]) + nextevent.endTime;
+    } else {
+        dateSTART = new Date(parseInt(nextevent.date.substring(0, 4)), parseInt(nextevent.date.substring(5, 7)), parseInt(nextevent.date.substring(8, 10)), parseInt(nextevent.beginTime.substring(0, 2)), parseInt(nextevent.beginTime.substring(2, 4)), 0, 0);
+        dateEND = new Date(parseInt(nextevent.date.substring(0, 4)), parseInt(nextevent.date.substring(5, 7)), parseInt(nextevent.date.substring(8, 10)), parseInt(nextevent.endTime.substring(0, 2)), parseInt(nextevent.endTime.substring(2, 4)), 0, 0);
+    }
+    return dateSTART < d && d < dateEND;
+}
+
+/**
  * Get the names of all events
  * @return array of all event names, sorted by date
  */
 function getAllEvents() {
     var d = new Date();
-    var currTimeSTRING = "" + d.getFullYear().toString() + " " + d.getMonth().toString() + " " + d.getDate().toString() + " " + d.getHours().toString() + d.getMinutes().toString();
-    var currDay = currTime.getDay(); //0 is Sunday, 6 is Saturday
-    var currMonth = d.getMonth();
-    var currDate = d.getDate();
     var currYear = d.getYear();
-    var currHour = d.getHours();
-    var currMinutes = d.getMinutes();
-    var currtime = "" + currHour.toString() + currMinutes.toString();
     var currstorage = JSON.parse(loadLocalStorage("events")); //obtain all currently stored events
     var toSort = [];
     var i = 0;
+    var nextevent;
+    var eventmonthdate;
+    var newDateSTRING;
     for (i = 0; i < currstorage.events.length(); i += 1) { //ensure that a event with the same name DNE
         //for sorting purposes, repeated events use next non-exception dates
-        var nextevent = currstorage.events[i];
+        nextevent = currstorage.events[i];
         if (nextevent.repeat) {
-            //First, find the difference between the current day of the week and the next scheduled repeat event
-            var eventdays = nextevent.daysRepeated;
-            var ddate = 0; //number of days difference
-            var matchingindex = -1; //in the case that the event takes place on the current day, this will match the index. I.E. Today is Wednesday, daysrepeated is MTWS, matchingindex = 2 for W.
-            var j;
-            for (j = 0; j < eventdays.length(); j += 1) { //for every day it repeats
-                var unit = eventdays.charAt(j); // get the value and test
-                //test: if the event occurs on a given day, ensure that the edge case where the event has already passed is caught
-                if (unit === "M") {
-                    ddate = Math.min(ddate, (0 - currDay) % 7);
-                    if ((0 - currDay) % 7 === 0) {matchingindex = j;}
-                } else if (unit === "T") {
-                    ddate = Math.min(ddate, (1 - currDay) % 7);
-                    if ((1 - currDay) % 7 === 0) {matchingindex = j;}
-                } else if (unit === "W") {
-                    ddate = Math.min(ddate, (2 - currDay) % 7);
-                    if ((2 - currDay) % 7 === 0) {matchingindex = j;}
-                } else if (unit === "R") {
-                    ddate = Math.min(ddate, (3 - currDay) % 7);
-                    if ((3 - currDay) % 7 === 0) {matchingindex = j;}
-                } else if (unit === "F") {
-                    ddate = Math.min(ddate, (4 - currDay) % 7);
-                    if ((4 - currDay) % 7 === 0) {matchingindex = j;}
-                } else if (unit === "S") {
-                    ddate = Math.min(ddate, (5 - currDay) % 7);
-                    if ((5 - currDay) % 7 === 0) {matchingindex = j;}
-                } else if (unit === "N") {
-                    ddate = Math.min(ddate, (6 - currDay) % 7);
-                    if ((6 - currDay) % 7 === 0) {matchingindex = j;}
-                }
-            }
-
-            //edge case where event is currently happening or happened earlier today
-            if (currtime < starttime && matchingindex > -1) {
-                var nextoccurance = eventdays.charAt((matchingindex + 1) % eventdays.length());
-                var nextoccurancenum = 0;
-                if (nextoccurance === "M") {nextoccurancenum = 0;}
-                else if (nextoccurance === "T") {nextoccurancenum = 1;}
-                else if (nextoccurance === "W") {nextoccurancenum = 2;}
-                else if (nextoccurance === "R") {nextoccurancenum = 3;}
-                else if (nextoccurance === "F") {nextoccurancenum = 4;}
-                else if (nextoccurance === "S") {nextoccurancenum = 5;}
-                else if (nextoccurance === "N") {nextoccurancenum = 6;}
-                ddate = (nextoccurancenum - currDay) % 7;
-            }
-
-            //Then, adjust months and days (dates) based off of leap years and month transitions.
-            var eventmonth = currMonth;
-            var eventdate = currDate;
-
-            if ((currMonth === 1 || currMonth === 3 || currMonth === 5 || currMonth === 7 || currMonth === 8 || currMonth === 10 || currMonth === 12) && currDate + ddate > 31) {
-                eventmonth += 1;
-                eventdate = (eventdate + ddate) % 31;
-            } else if ((currMonth === 4 || currMonth === 6 || currMonth === 9 || currMonth === 11) && currDate + ddate > 30) {
-                eventmonth += 1;
-                eventdate = (eventdate + ddate) % 39;
-            } else if (currMonth === 2 && currYear % 4 === 0 && currDate + ddate > 29) {
-                eventmonth += 1;
-                eventdate = (eventdate + ddate) % 29;
-            } else if (currMonth === 2 && currYear % 4 !== 0 && currDate + ddate > 28) {
-                eventmonth += 1;
-                eventdate = (eventdate + ddate) % 28;
-            }
+            eventmonthdate = getRepeatingEventMonthDate(d, nextevent);
 
             //Generate the date string for comparison
-            var newDateSTRING = "" + currYear + " " + zerofillTwoDigits(eventmonth) + "" + zerofillTwoDigits(eventdate) + nextevent.startTime;
+            newDateSTRING = "" + currYear + " " + zerofillTwoDigits(eventmonthdate[0]) + "" + zerofillTwoDigits(eventmonthdate[1]) + nextevent.startTime;
 
             //Finally, push to the toSort array
             toSort.push({name: nextevent.name, date: newDateSTRING});
 
         } else {
             //Date of the event
-            var newDateSTRING = nextevent.date + "" + nextevent.startTime;
+            newDateSTRING = nextevent.date + "" + nextevent.startTime;
             toSort.push({name: nextevent.name, date: newDateSTRING});
         }
 
         //now do the sort. Insertion Sort.
-        int s;
-        int t;
-        for (s = 1; s < toSort.length()) {
+        var s;
+        var t;
+        for (s = 1; s < toSort.length(); s += 1) {
             var temp = toSort[s];
             t = s - 1;
             while (t >= 0 && toSort[t].date > temp.date) {
@@ -128,6 +89,97 @@ function getAllEvents() {
         return toSort;
     }
 
+}
+
+/**
+ * Using current date, determine the month and date of the next occurance of a repeating event
+ * @return array consisting of month and day (date) of the event's next occurance
+ */
+function getRepeatingEventMonthDate(d, nextevent) {
+    var currDay = d.getDay(); //0 is Sunday, 6 is Saturday
+    var currYear = d.getYear();
+    var currMonth = d.getMonth();
+    var currDate = d.getDate();
+    var currHour = d.getHours();
+    var currMinutes = d.getMinutes();
+    var currtime = "" + currHour.toString() + currMinutes.toString();
+    //First, find the difference between the current day of the week and the next scheduled repeat event
+    var eventdays = nextevent.daysRepeated;
+    var ddate = 0; //number of days difference
+    var matchingindex = -1; //in the case that the event takes place on the current day, this will match the index. I.E. Today is Wednesday, daysrepeated is MTWS, matchingindex = 2 for W.
+    var j;
+    var unit; //the specific day of the week identifier currently chosen
+    for (j = 0; j < eventdays.length(); j += 1) { //for every day it repeats
+        unit = eventdays.charAt(j); // get the value and test
+        //test: if the event occurs on a given day, ensure that the edge case where the event has already passed is caught
+        if (unit === "M") {
+            ddate = Math.min(ddate, (0 - currDay) % 7);
+            if ((0 - currDay) % 7 === 0) {matchingindex = j;}
+        } else if (unit === "T") {
+            ddate = Math.min(ddate, (1 - currDay) % 7);
+            if ((1 - currDay) % 7 === 0) {matchingindex = j;}
+        } else if (unit === "W") {
+            ddate = Math.min(ddate, (2 - currDay) % 7);
+            if ((2 - currDay) % 7 === 0) {matchingindex = j;}
+        } else if (unit === "R") {
+            ddate = Math.min(ddate, (3 - currDay) % 7);
+            if ((3 - currDay) % 7 === 0) {matchingindex = j;}
+        } else if (unit === "F") {
+            ddate = Math.min(ddate, (4 - currDay) % 7);
+            if ((4 - currDay) % 7 === 0) {matchingindex = j;}
+        } else if (unit === "S") {
+            ddate = Math.min(ddate, (5 - currDay) % 7);
+            if ((5 - currDay) % 7 === 0) {matchingindex = j;}
+        } else if (unit === "N") {
+            ddate = Math.min(ddate, (6 - currDay) % 7);
+            if ((6 - currDay) % 7 === 0) {matchingindex = j;}
+        }
+    }
+
+    var nextoccurance; //the character for the day of the week of the next occurance of the event 
+    var nextoccurancenum; //the day ID of the next occurance of the event after the current day
+
+    //edge case where event is currently happening or happened earlier today
+    if (currtime < nextevent.startTime && matchingindex > -1) {
+        nextoccurance = eventdays.charAt((matchingindex + 1) % eventdays.length());
+        nextoccurancenum = 0;
+        if (nextoccurance === "M") {
+            nextoccurancenum = 0;
+        } else if (nextoccurance === "T") {
+            nextoccurancenum = 1;
+        } else if (nextoccurance === "W") {
+            nextoccurancenum = 2;
+        } else if (nextoccurance === "R") {
+            nextoccurancenum = 3;
+        } else if (nextoccurance === "F") {
+            nextoccurancenum = 4;
+        } else if (nextoccurance === "S") {
+            nextoccurancenum = 5;
+        } else if (nextoccurance === "N") {
+            nextoccurancenum = 6;
+        } 
+        ddate = (nextoccurancenum - currDay) % 7;
+    }
+
+    //Then, adjust months and days (dates) based off of leap years and month transitions.
+    var eventmonth = currMonth;
+    var eventdate = currDate;
+
+    if ((currMonth === 1 || currMonth === 3 || currMonth === 5 || currMonth === 7 || currMonth === 8 || currMonth === 10 || currMonth === 12) && currDate + ddate > 31) {
+        eventmonth += 1;
+        eventdate = (eventdate + ddate) % 31;
+    } else if ((currMonth === 4 || currMonth === 6 || currMonth === 9 || currMonth === 11) && currDate + ddate > 30) {
+        eventmonth += 1;
+        eventdate = (eventdate + ddate) % 39;
+    } else if (currMonth === 2 && currYear % 4 === 0 && currDate + ddate > 29) {
+        eventmonth += 1;
+        eventdate = (eventdate + ddate) % 29;
+    } else if (currMonth === 2 && currYear % 4 !== 0 && currDate + ddate > 28) {
+        eventmonth += 1;
+        eventdate = (eventdate + ddate) % 28;
+    }
+
+    return [eventmonth, eventdate];
 }
 
 /* **************** CRUD Functions **************** */

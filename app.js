293e2611671225d74a9d77c519ba0072825ebbed
Andrew Fan <andrewsuicune@gmail.com>
2016-10-22 10:46:25 -0400

    Added createNewList

--- app.js
+++ app.js
@@ -4,14 +4,55 @@
 
 /* **************** Feature Functions **************** */
 
+/**
+ * Create a new list and save it to local storage
+ */
+function createNewList(String name, String type, String[] sites) {
+    var currstorage = JSON.parse(loadLocalStorage("lists")); //obtain all currently stored lists
+    var i = 0;
+    for (i = 0; i < currstorage.lists.length(); i++) { //ensure that a list with the same name DNE
+        if (currstorage.lists[i].name === name) {
+            alert("Failed to create new list - the provided name belongs to a list that already exists. Please choose a different name.");
+        }
+    }
+    var newlist = {
+        "name" : name,
+        "type" : type,
+        "sites" : sites
+    };
+    currstorage.lists.push(newlist);
+    setLocalStorage("lists", JSON.stringify(currstorage));
+}
+
+/**
+ * Read an existing list from local storage
+ */
+function ReadNewList(String name, String type, String[] sites) {
+    
+}
+
 
 
 /* **************** Helper Functions **************** */
 
-function setSessionStorage(String key, String value) {
+/**
+ * See if the browser supports local storage
+ * @return true if supported, false otherwise
+ */
+function checkBrowserCompatibility() {
+    return typeof(Storage) !== "undefined";
+}
+
+/**
+ * Set local storage with given key-value pair
+ */
+function setLocalStorage(String key, String value) {
     localStorage.setItem(key, value);
 }
 
-function loadSessionStorage(String key) {
+/**
+ * Obtain value in local storage with given key
+ */
+function loadLocalStorage(String key) {
     return localStorage.getItem(key);
 }

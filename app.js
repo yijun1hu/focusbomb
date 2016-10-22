115a409ac55d07cd13a2987d02a0028d6bb175b8
Andrew Fan <andrewsuicune@gmail.com>
2016-10-22 11:07:09 -0400

    CRUD done for lists

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /* **************** Setup Functions **************** */
 
 
@@ -7,19 +9,19 @@
 /**
  * Create a new list and save it to local storage
  */
-function createNewList(String name, String type, String color, String[] sites) {
+function createNewList(name, type, color, sites) {
     var currstorage = JSON.parse(loadLocalStorage("lists")); //obtain all currently stored lists
     var i = 0;
-    for (i = 0; i < currstorage.lists.length(); i++) { //ensure that a list with the same name DNE
+    for (i = 0; i < currstorage.lists.length(); i += 1) { //ensure that a list with the same name DNE
         if (currstorage.lists[i].name === name) {
             alert("Failed to create new list - the provided name belongs to a list that already exists. Please choose a different name.");
         }
     }
     var newlist = {
-        "name" : name,
-        "type" : type,
-        "color" : color,
-        "sites" : sites
+        "name": name,
+        "type": type,
+        "color": color,
+        "sites": sites
     };
     currstorage.lists.push(newlist);
     setLocalStorage("lists", JSON.stringify(currstorage));
@@ -29,16 +31,62 @@ function createNewList(String name, String type, String color, String[] sites) {
  * Read an existing list from local storage
  * @return javascript object referring to 
  */
-function readExistingList(String name) {
+function readExistingList(name) {
     var currstorage = JSON.parse(loadLocalStorage("lists"));
     var i = 0;
-    for (i = 0; i < currstorage.lists.length(); i++) { //find the specific list
+    for (i = 0; i < currstorage.lists.length(); i += 1) { //find the specific list
         if (currstorage.lists[i].name === name) {
             return currstorage.lists[i];
         }
-    }   
+    }
+    alert("Failed to read list - the provided name does not match any existing lists.");
 }
 
+/**
+ * Update an existing list from local storage
+ */
+function updateExistingList(name, type, color, sites) {
+    var currstorage = JSON.parse(loadLocalStorage("lists"));
+    var targetlistindex = -1;
+    var i = 0;
+    for (i = 0; i < currstorage.lists.length(); i += 1) { //find the specific list
+        if (currstorage.lists[i].name === name) {
+            targetlistindex = i;
+        }
+    }
+    if (targetlistindex === -1) {
+        alert("Failed to update list - the provided name does not match any existing lists.");
+    } else {
+        var newlist = {
+            "name": name,
+            "type": type,
+            "color": color,
+            "sites": sites
+        };
+        currstorage.lists[targetlistindex] = newlist;
+        setLocalStorage("lists", JSON.stringify(currstorage));
+    }
+}
+
+/**
+ * Delete an existing list from local storage
+ */
+function deleteExistingList(name) {
+    var currstorage = JSON.parse(loadLocalStorage("lists"));
+    var found = false;
+    var i = 0;
+    for (i = 0; i < currstorage.lists.length(); i += 1) { //find the specific list
+        if (currstorage.lists[i].name === name) {
+            currstorage.lists = array.splice(i, 1); //remove the specific list
+            found = true;
+        }
+    }
+    if (!found) {
+        alert("Failed to delete list - the provided name does not match any existing lists.");
+    } else {
+        setLocalStorage("lists", JSON.stringify(currstorage));
+    }
+}
 
 
 /* **************** Helper Functions **************** */
@@ -54,13 +102,13 @@ function checkBrowserCompatibility() {
 /**
  * Set local storage with given key-value pair
  */
-function setLocalStorage(String key, String value) {
+function setLocalStorage(key, value) {
     localStorage.setItem(key, value);
 }
 
 /**
  * Obtain value in local storage with given key
  */
-function loadLocalStorage(String key) {
+function loadLocalStorage(key) {
     return localStorage.getItem(key);
 }

9dff165e79e794a3b3b5761210b9acc8801f9c5b
Andrew Fan <andrewsuicune@gmail.com>
2016-10-22 23:28:02 -0400

    Added console log messages whenever a page is supposed to be blocked

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 //Following was adapted from https://github.com/tetsuwo/website-blocker-chrome.ext
 chrome.tabs.onCreated.addListener(function(tab) {
     if (determineIsBlocked(tab.url)) {
+        console.log("chrome.tabs.onCreated.addListener - blocking page");
         blockPage(tab.id, tab.url);
     }
 });
@@ -8,6 +9,7 @@ chrome.tabs.onCreated.addListener(function(tab) {
 chrome.tabs.onActivated.addListener(function(info) {
     chrome.tabs.get(info.tabId, function(tab) {
         if (determineIsBlocked(tab.url)) {
+            console.log("chrome.tabs.onActivated.addListener - blocking page");
             blockPage(tab.id, tab.url);
         }
     });
@@ -16,6 +18,7 @@ chrome.tabs.onActivated.addListener(function(info) {
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === "loading") {
         if (determineIsBlocked(tab.url)) {
+            console.log("chrome.tabs.onUpdated.addListener - blocking page");
             blockPage(tab.id, tab.url);
         }
         return;

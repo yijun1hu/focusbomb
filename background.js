366fe69d79c04772a68c0878837a78c3a82b3212
Andrew Fan <andrewsuicune@gmail.com>
2016-10-23 03:43:17 -0400

    Removed alerts

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 //Following was adapted from https://github.com/tetsuwo/website-blocker-chrome.ext
 chrome.tabs.onCreated.addListener(function(tab) {
     if (determineIsBlocked(tab.url)) {
-        alert("A");
         console.log("chrome.tabs.onCreated.addListener - blocking page");
         blockPage(tab.id, tab.url);
     }
@@ -10,7 +9,6 @@ chrome.tabs.onCreated.addListener(function(tab) {
 chrome.tabs.onActivated.addListener(function(info) {
     chrome.tabs.get(info.tabId, function(tab) {
         if (determineIsBlocked(tab.url)) {
-            alert("B");
             console.log("chrome.tabs.onActivated.addListener - blocking page");
             blockPage(tab.id, tab.url);
         }
@@ -20,7 +18,6 @@ chrome.tabs.onActivated.addListener(function(info) {
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === "loading") {
         if (determineIsBlocked(tab.url)) {
-            alert("C");
             console.log("chrome.tabs.onUpdated.addListener - blocking page");
             blockPage(tab.id, tab.url);
         }
